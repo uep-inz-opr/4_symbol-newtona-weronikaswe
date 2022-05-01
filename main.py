4cf283d9dd8880296f54fc2b1e4af82462593701
Werka <69664@student.ue.poznan.pl>
2022-05-01 16:25:40 +0200

    no niestety

--- main.py
+++ main.py
@@ -2,17 +2,29 @@ import math
 
 new = input()
 print(new)
-#if len(new)==3:
-#    n = int(new[0])
-#    k = int(new[2])
-#    if n == 1 or n == k:
-#        print(1)
-#    if k > n:
-#        print(0)
-#    else:
-#        x = math.factorial(n)
-#        y = math.factorial(k)
-#        sym = x//(y*(n-k))
-#        print((sym))
-#else:
-#    print(0)
+if len(new)==3:
+    n = int(new[0])
+    k = int(new[2])
+    if n == 1 or n == k:
+        print(1)
+    if k > n:
+        print(0)
+    else:
+        x = math.factorial(n)
+        y = math.factorial(k)
+        sym = x//(y*(n-k))
+        print((sym))
+elif len(new)==5:
+    n = int(new[0:1])
+    k = int(new[3:4])
+    if n == 1 or n == k:
+        print(1)
+    if k > n:
+        print(0)
+    else:
+        x = math.factorial(n)
+        y = math.factorial(k)
+        sym = x//(y*(n-k))
+        print((sym))
+else:
+    print(0)
