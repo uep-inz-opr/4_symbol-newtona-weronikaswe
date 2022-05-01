8493f38c6572a43ee65676872190c51b7c130a29
Werka <69664@student.ue.poznan.pl>
2022-05-01 17:10:07 +0200

    test

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import math
 
 new = input()
-print(len(new))
+
 if len(new)==3:
     n = int(new[0])
     k = int(new[2])
@@ -17,14 +17,16 @@ if len(new)==3:
 elif len(new)==5:
     n = int(new[0:1])
     k = int(new[3:4])
-    if n == k:
-        print(1)
-    if k > n:
-        print(0)
-    else:
-        x = math.factorial(n)
-        y = math.factorial(k)
-        sym = x//(y*(n-k))
-        print((sym))
-else:
-    print(0)
+    print(n)
+    print(k)
+#    if n == k:
+#        print(1)
+#    if k > n:
+#        print(0)
+#    else:
+#        x = math.factorial(n)
+#        y = math.factorial(k)
+#        sym = x//(y*(n-k))
+#        print((sym))
+#else:
+#    print(0)
