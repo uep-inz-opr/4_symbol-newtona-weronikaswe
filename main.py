3dfb8f0b7d0d65b4601289f23c06402226383f3d
Werka <69664@student.ue.poznan.pl>
2022-05-01 22:49:52 +0200

    Newton o co ci chodzi

--- main.py
+++ main.py
@@ -1,23 +1,11 @@
 import math
 
-
 def ile_symbol(n,k):
-#    if len(symbol) == 3:
-#        n = symbol[0]
-#        k = symbol[2]
     x = math.factorial(n)
     y = math.factorial(k)
     z = math.factorial(n-k)
     wynik = x/(y*z)
     return wynik
-#    elif len(symbol) == 5:
-#        n = int(symbol[0] + symbol[1])
-#        k = int(symbol[3] + symbol[4])
-#        x = math.factorial(n)
-#        y = math.factorial(k)
-#        z = math.factorial(n-k)
-#        wynik = x / (y * z)
-#        return wynik
 
 symbol = input()
 if len(symbol) == 3:
