73de5e76a21c9a43cd47262af1c1a0dca829619c
Werka <69664@student.ue.poznan.pl>
2022-05-01 17:12:01 +0200

    test

--- main.py
+++ main.py
@@ -15,18 +15,18 @@ if len(new)==3:
         sym = x//(y*(n-k))
         print((sym))
 elif len(new)==5:
-    n = int(new[0:1])
-    k = int(new[3:4])
+    n = int(new[0],new[1])
+    k = int(new[3],new[4])
     print(n)
     print(k)
-#    if n == k:
-#        print(1)
-#    if k > n:
-#        print(0)
-#    else:
-#        x = math.factorial(n)
-#        y = math.factorial(k)
-#        sym = x//(y*(n-k))
-#        print((sym))
-#else:
-#    print(0)
+    if n == k:
+        print(1)
+    if k > n:
+        print(0)
+    else:
+        x = math.factorial(n)
+        y = math.factorial(k)
+        sym = x//(y*(n-k))
+        print((sym))
+else:
+    print(0)
