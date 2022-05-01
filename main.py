ab5f7c438199a38705b503109700ee8a34d6690c
Werka <69664@student.ue.poznan.pl>
2022-05-01 22:45:03 +0200

    liczenie Newtona jest super

--- main.py
+++ main.py
@@ -1,28 +1,32 @@
 import math
 
 
-def symbol(n,k):
-    if len(symbol) == 3:
-        n = symbol[0]
-        k = symbol[2]
-        x = math.factorial(n)
-        y = math.factorial(k)
-        z = math.factorial(n-k)
-        wynik = x/(y*z)
-        return wynik
-    elif len(symbol) == 5:
-        n = int(symbol[0] + symbol[1])
-        k = int(symbol[3] + symbol[4])
-        x = math.factorial(n)
-        y = math.factorial(k)
-        z = math.factorial(n-k)
-        wynik = x / (y * z)
-        return wynik
+def ile_symbol(n,k):
+#    if len(symbol) == 3:
+#        n = symbol[0]
+#        k = symbol[2]
+    x = math.factorial(n)
+    y = math.factorial(k)
+    z = math.factorial(n-k)
+    wynik = x/(y*z)
+    return wynik
+#    elif len(symbol) == 5:
+#        n = int(symbol[0] + symbol[1])
+#        k = int(symbol[3] + symbol[4])
+#        x = math.factorial(n)
+#        y = math.factorial(k)
+#        z = math.factorial(n-k)
+#        wynik = x / (y * z)
+#        return wynik
 
-oblicz = input()
-n=oblicz[0]
-k=oblicz[2]
-wynik=symbol(n,k)
+symbol = input()
+if len(symbol) == 3:
+    n = symbol[0]
+    k = symbol[2]
+elif len(symbol) == 5:
+    n = int(symbol[0] + symbol[1])
+    k = int(symbol[3] + symbol[4])
+wynik=ile_symbol(n,k)
 print(wynik)
 
 
